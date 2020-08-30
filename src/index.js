f52de1285683ead5d43fad75a3ce6ab17ef63b02
Konstantin Arakantsev <kostya3312@gmail.com>
2020-08-30 10:54:23 +0300

    use event delegation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const accordions = document.querySelectorAll('.accordion')
+const accordionContainer = document.querySelector('.accordion-container')
 
-accordions.forEach(accordion => {
-  const header = accordion.querySelector('.accordion__header')
+const IS_OPEN_CLASS = 'is-open'
 
-  header.addEventListener('click', () => {
-    accordion.classList.toggle('is-open')
-  })
+accordionContainer.addEventListener('click', e => {
+  const accordionHeader = e.target.closest('.accordion__header')
+  if (accordionHeader) {
+    const accordion = accordionHeader.parentElement
+    accordion.classList.toggle(IS_OPEN_CLASS)
+  }
 })
