2848bf8bd96d010e68270b6caab486babcd00a7c
Konstantin Arakantsev <kostya3312@gmail.com>
2020-08-31 10:14:38 +0300

    add content height animation

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ const IS_OPEN_CLASS = 'is-open'
 
 accordionContainer.addEventListener('click', e => {
   const accordionHeader = e.target.closest('.accordion__header')
+
   if (accordionHeader) {
     const accordion = accordionHeader.parentElement
+    const accordionContent = accordionHeader.nextElementSibling
+    const accordionInner = accordionContent.children[0]
+
+    const height = accordionInner.getBoundingClientRect().height
+
     accordion.classList.toggle(IS_OPEN_CLASS)
+
+    if (accordion.classList.contains(IS_OPEN_CLASS)) {
+      accordionContent.style.height = height + 'px'
+    } else {
+      accordionContent.style.height = 0;
+    }
+
   }
 })
