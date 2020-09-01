9b709d4845d393eea8ab8c8459b1796fea4153e1
Konstantin Arakantsev <kostya3312@gmail.com>
2020-09-01 15:22:13 +0300

    refactor: extract functions

--- src/index.js
+++ src/index.js
@@ -2,19 +2,40 @@ const accordionContainer = document.querySelector('.accordion-container')
 
 const IS_OPEN_CLASS = 'is-open'
 
+/**
+ * Finds the height of accordion content
+ * @param {HTMLElement} accordion The accordion
+ * @returns {Number} Height of accordion content in px
+ */
+const getContentHeight = accordion => {
+  const accordionInner = accordion.querySelector('.accordion__inner')
+
+  if (!accordion.classList.contains(IS_OPEN_CLASS)) return 0
+  return accordionInner.getBoundingClientRect().height
+}
+
+/**
+ * Set height of accordion
+ * @param {*} accordion The accordion
+ * @param {*} height Height of accordion inner content in px
+ */
+const setContentHeight = (accordion, height) => {
+  const accordionContent = accordion.querySelector('.accordion__content')
+  accordionContent.style.height = `${height}px`
+}
+
+// CODE
+
 accordionContainer.addEventListener('click', e => {
   const accordionHeader = e.target.closest('.accordion__header')
-
   if (!accordionHeader) return
 
   const accordion = accordionHeader.parentElement
+
+  // toggle open class
   accordion.classList.toggle(IS_OPEN_CLASS)
 
-  const accordionContent = accordionHeader.nextElementSibling
-  const accordionInner = accordionContent.children[0]
-  const { height } = accordionInner.getBoundingClientRect()
-
-  accordionContent.style.height = accordion.classList.contains(IS_OPEN_CLASS)
-    ? `${height}px`
-    : 0
+  // set content height
+  const height = getContentHeight(accordion)
+  setContentHeight(accordion, height)
 })
